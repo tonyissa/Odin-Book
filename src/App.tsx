170e80bec9920c6965f52d972d2412a11e0ab865
antwonthedamaja <52683702+antwonthedamaja@users.noreply.github.com>
2023-12-17 16:45:36 -0500

    Code login functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Redirect from './pages/Redirect/index';
+import CreateAccount from './pages/CreateAccount';
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Redirect />}>
-
+        <Route path='/'>
+          <Route index element={<Redirect />} />
+          <Route path='create-account' element={<CreateAccount />} />
         </Route>
       </Routes>
     </BrowserRouter>
--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,5 +1,41 @@
+import { useState } from 'react'
+import { useNavigate, Link } from 'react-router-dom';
+
 export default function Login() {
-    return <div>
-        box
+    const [login, setLogin] = useState({ user: '', password: '' });
+    const [loading, setLoading] = useState(false);
+    const navigate = useNavigate();
+
+    async function handleUsernameLogin() {
+        try {
+            setLoading(true);
+            const response = await fetch('http://localhost:3000/api/login', {
+                method: 'post',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                credentials: 'include',
+                mode: 'cors',
+                body: JSON.stringify({ login: login.user, password: login.password })
+            })
+            setLoading(false)
+            if (response.status === 200) {
+                navigate('.')
+            }
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+    return <div className="h-screen flex justify-center items-center">
+        <div className="border p-4 flex flex-col h-fit">
+            <input type="text" placeholder="Email" value={login.user} onChange={e => setLogin({ ...login, user: e.target.value })} />
+            <input type="password" placeholder="Password" value={login.password} onChange={e => setLogin({ ...login, password: e.target.value })} />
+            <button onClick={handleUsernameLogin} disabled={loading}>Log in</button>
+            <button disabled={loading}>Log in with github</button>
+            <button disabled={loading}>Log in with google</button>
+            <hr />
+            <button><Link to='create-account'>Create account</Link></button>
+        </div>
     </div>
 }
